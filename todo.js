b3023f4b473ee92d2855c34aba4c87cc4cac9e51
jsurrati <jsurrati@abb18e54-ca02-4809-84f6-80c156c1dc92>
2008-09-29 14:39:51 +0000

    Move configuration options for the JavaScript code to a separate config.js file (Todo #1064)
    git-svn-id: svn+ssh://svn.pgengler.net/srv/svn/personaltodo/trunk@58 abb18e54-ca02-4809-84f6-80c156c1dc92

--- todo.js
+++ todo.js
@@ -1,8 +1,3 @@
-var index_url = 'http://personal.pgengler.net/todo/';
-var base_url  = index_url + 'ajax.cgi';
-
-var undated_last = 0;
-var use_mark     = 0;
 var remove       = [];
 var moving       = false;
 
