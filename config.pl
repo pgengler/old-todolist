ce76bdfd7a4c45a4fb4d574bc87df4fcf8a0465d
pgengler <pgengler@abb18e54-ca02-4809-84f6-80c156c1dc92>
2009-06-03 17:36:35 +0000

    Move to separate database (Todo #1402)
    git-svn-id: svn+ssh://svn.pgengler.net/srv/svn/personaltodo/trunk@103 abb18e54-ca02-4809-84f6-80c156c1dc92

--- config.pl
+++ config.pl
@@ -7,10 +7,10 @@ our $url          = 'http://personal.pgengler.net/todo/';
 
 # Database connection info
 our $db_host      = 'localhost';
-our $db_user      = 'jsurrati_pgphil'; # username used to connect to the database
-our $db_pass      = 'phil11!!';        # password used to connect to the database
-our $db_name      = 'jsurrati_pgphil'; # name of the database to use (NOTE: DATABASE, not TABLES)
-#our $db_name     = 'jsurrati_pgdev';
+our $db_user      = 'jsurrati_pgptodo'; # username used to connect to the database
+our $db_pass      = 'todo11!!';         # password used to connect to the database
+our $db_name      = 'jsurrati_pgptodo'; # name of the database to use (NOTE: DATABASE, not TABLES)
+#our $db_name     = 'jsurrati_pgptododev';
 our $db_prefix    = '';
 
 #######
